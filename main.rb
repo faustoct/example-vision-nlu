eeffd76f55bfe5e75f22591303bd64b8e5aaacdf
faustoct <fausto.alemao@gmail.com>
2019-04-11 21:08:30 -0300

    Update and rename analyze.rb to main.rb

--- main.rb
+++ main.rb
@@ -24,3 +24,35 @@ result[:analyzed] = response
 result[:raw_text]=response["responses"].first["fullTextAnnotation"]["text"]
 result[:sentences]=response["responses"].first["fullTextAnnotation"]["text"].split("\n")
 result[:sentence]=response["responses"].first["fullTextAnnotation"]["text"].split("\n").join(" ")
+
+#Classify
+params = {
+  "document":
+  {
+    "content": "#{result[:sentence]}",
+    "type": "PLAIN_TEXT",
+    "language": "pt-br"
+  },
+  "features":
+  {
+    "extractEntities": true
+  }
+}
+response = HTTParty.post(
+  "https://language.googleapis.com/v1/documents:annotateText?key=AIzaSyAi0QezwxaouYQVMhAre2kucrSY6lus5rU",
+  headers: { 'Content-Type' => 'application/json' },
+  body: params.to_json
+)
+
+result[:entities].each do |e|
+  puts "#{e["name"]} : #{e["type"]}"
+end
+
+#output
+BACK-END : CONSUMER_GOOD
+parte : OTHER
+DİZENDO : CONSUMER_GOOD
+DE MEMORIA : OTHER
+EM PROGRAMAÇÃO : CONSUMER_GOOD
+6 : NUMBER
+2 : NUMBER
