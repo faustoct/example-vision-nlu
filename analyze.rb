b79b358c7d890cabb93bf9dce54a0a9eb13c832b
faustoct <fausto.alemao@gmail.com>
2019-04-11 21:02:24 -0300

    Create analyze.rb

---
+++ analyze.rb
@@ -0,0 +1,28 @@
+#Analyze image
+media = "https://i.ytimg.com/vi/DGU1awKrNiA/hqdefault.jpg"
+params = {
+  "requests":
+  [
+    {
+      "features":
+      [
+        { "maxResults": 50, "type": "TEXT_DETECTION" },
+      ],
+      "image":
+      {
+        "source": { "imageUri": "#{media}" }
+      },
+    }
+  ]
+}
+response = HTTParty.post(
+  "https://vision.googleapis.com/v1/images:annotate?key={REPLACE_YOUR_API_KEY}",
+  headers: { 'Content-Type' => 'application/json' },
+  body: params.to_json
+)
+result[:analyzed] = response
+result[:raw_text]=response["responses"].first["fullTextAnnotation"]["text"]
+result[:sentences]=response["responses"].first["fullTextAnnotation"]["text"].split("\n")
+result[:sentence]=response["responses"].first["fullTextAnnotation"]["text"].split("\n").join(" ")
+
+#Classify image
